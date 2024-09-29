4432a900e3734eb0d7751a6d67a291d26fdc3e11
manishamali1525 <manishamali1525@users.noreply.github.com>
2024-09-29 20:52:31 +0530

    added files

---
+++ src/components/ReactMemo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReactMemo = ({text}) =>{
+    return(
+        <>
+        <h2>React.memo Component</h2>
+        <p>Current input: {text}</p>
+
+        </>
+    )
+}
+
+export default ReactMemo;
---
+++ src/components/UseMemo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UseMemo = ({count}) =>{
+    return(
+        <div>
+            <h2>UseMemo Component</h2>
+            <p>Total todos: {count}</p>
+        </div>
+    )
+}
+
+export default UseMemo;
